1ee0805e9959098007e2a2f02d656ba1c6f7de28
agakrawczyk <agnieszkakrawczyk11@gmail.com>
2016-06-17 13:15:32 +0200

    Add files via upload

--- Heat_map.py
+++ Heat_map.py
@@ -1,14 +1,15 @@
+"""
+Creates a heatmap on the basis of the data set  from http://hadobs.metoffice.com/hadsst3/ containing inf about anomalies
+in sea surface temperature.
+
+"""
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from collections import OrderedDict
-import numpy as np
 
-data = OrderedDict({})
-month_dict = OrderedDict({'01': 'Jan', '02': 'Feb', '03': 'Mar', '04': 'Apr', '05': 'May', '06': 'Jun',
-                          '07': 'Jul', '08': 'Aug', '09': 'Sep', '10': 'Oct', '11': 'Nov', '12': 'Dec'})
+data = {}
 
-with open('HadSST.3.1.1.0_monthly_globe_ts.txt', 'r') as f:
+with open('input/HadSST.3.1.1.0_monthly_globe_ts.txt', 'r') as f:
     for row in f:
         parts = row.strip().split()
         temp = float(parts[1])
@@ -21,10 +22,14 @@ with open('HadSST.3.1.1.0_monthly_globe_ts.txt', 'r') as f:
 
         data[year][month] = temp
 
+
 temp_df = pd.DataFrame(data)
-print(temp_df)
+
 ax = sns.heatmap(temp_df)
-ax.set_xticklabels(labels=data, rotation=30, size=8)
+ax.set_xticklabels(labels=[str(i) if i % 50 == 0 else '' for i in range(1850, 2015)], rotation=30, size=8)
+ax.set_yticklabels(labels=['Dec', 'Nov', 'Oct', 'Sep', 'Aug', 'Jul', 'Jun', 'May', 'Apr', 'Mar', 'Feb', 'Jan'],
+                   rotation=0, size=8)
+ax.set_title('Sea surface temperature anomalies (monthly, globe) in years 1850-2015')
 plt.draw()
 plt.show()
 
